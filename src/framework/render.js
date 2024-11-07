49378f2101b05da14fc94e621b9689e8c8096e3d
User <user@MacBook-Air--Kambole.local>
2024-11-07 18:48:43 +0300

    new3

--- src/framework/render.js
+++ src/framework/render.js
@@ -9,14 +9,13 @@ const RenderPosition = {
   function createElement(template) {
     const newElement = document.createElement('div');
     newElement.innerHTML = template;
-  
-  
+    
     return newElement.firstElementChild;
   }
   
   
   function render(component, container, place = RenderPosition.BEFOREEND) {
-  container.insertAdjacentElement(place, component.getElement());
+    container.insertAdjacentElement(place, component.getElement());
   }
   
   
--- src/main.js
+++ src/main.js
@@ -3,10 +3,23 @@ import {render, RenderPosition} from './framework/render.js';
 
 import FormAddTaskComponent from './views/form-add-task-component.js';
 
+import TaskListComponent from './views/task-list-component.js';
+
+const formContainer = document.querySelector('.add-task');
 const bodyContainer= document.querySelector('.board-app');
+const taskListContainer = document.querySelector('.taskboard');
 
 render(new HeaderComponent(), bodyContainer, RenderPosition.BEFOREBEGIN);
-const formContainer = document.querySelector('.add-task');
+
 
 render(new FormAddTaskComponent(), formContainer);
+
+var i=0
+while (i<4) {
+    render(new TaskListComponent(), taskListContainer,RenderPosition.AFTERBEGIN);
+    i+=1;}
+
+
+
+
 
